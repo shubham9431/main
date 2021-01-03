be7ca6dddd68aa3b61896d2d047d31a078dd96c4
shubham <shubhamurkude31@gmail.com>
2021-01-03 16:14:24 +0530

    merge

---
+++ myproject/src/myproject/cjc/LoginServlet.java
@@ -0,0 +1,15 @@
+package myproject.cjc;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+protected void service(HttpServletRequest args0,HttpServletResponse args1)throws IOException
+{
+System.out.println("hello");	
+}
+
+}
